25482ea76350aa397a0ee7711e8e9bae31642cf3
Antonin Grenier <drixion05@gmail.com>
2025-11-19 16:48:10 +0100

    added display logic on headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,56 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import "../assets/styles/Header.css";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 function Header() {
-  return (
-    <Navbar bg="light" data-bs-theme="light">
-      <Container>
-        <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/">
-            Accueil
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/publiques">
-            Offres Publiques
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/professionnelles">
-            Offres Professionnelles
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/inscription">
-            Inscription
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/connexion">
-            Connexion
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/deconnexion">
-            Déconnexion
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+	const location = useLocation();
+	const auth = useSelector((state) => state.auth);
+
+	const getValidToken = () => {
+		const isValid = auth.token && new Date(auth.expiresAt) > new Date();
+		return isValid;
+	};
+
+	const [isConnected, setIsConnected] = useState(null);
+
+	useEffect(() => {
+		setIsConnected(getValidToken());
+	}, [location]);
+
+	return (
+		<Navbar bg="light" data-bs-theme="light">
+			<Container>
+				<Nav className="ms-auto">
+					<Nav.Link as={NavLink} to="/">
+						Accueil
+					</Nav.Link>
+					<Nav.Link as={NavLink} to="/offres/publiques">
+						Offres Publiques
+					</Nav.Link>
+
+					<Nav.Link as={NavLink} to="/offres/professionnelles">
+						Offres Professionnelles
+					</Nav.Link>
+					{!isConnected && (
+						<>
+							<Nav.Link as={NavLink} to="/inscription">
+								Inscription
+							</Nav.Link>
+							<Nav.Link as={NavLink} to="/connexion">
+								Connexion
+							</Nav.Link>
+						</>
+					)}
+					{isConnected && (
+						<Nav.Link as={NavLink} to="/deconnexion">
+							Déconnexion
+						</Nav.Link>
+					)}
+				</Nav>
+			</Container>
+		</Navbar>
+	);
 }
 export default Header;
