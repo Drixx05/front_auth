1c655744658736956208e292793d710def19bea5
Antonin Grenier <drixion05@gmail.com>
2025-11-19 10:33:39 +0100

    logout WIP

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,20 +1,28 @@
-import { useEffect } from 'react';
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const Logout = () => {
-   useEffect(() => {
-    const handleLogout = async () => {
-      // (1) Appel API pour notifier la déconnexion
+	const navigate = useNavigate();
+	useEffect(() => {
+		const handleLogout = async () => {
+			await fetch("/logout", {
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${
+						JSON.parse(localStorage.getItem("auth"))?.token
+					}`,
+				},
+			});
 
-      // (2) Suppression du token côté frontend
+			localStorage.removeItem("auth");
 
-      // (3) Redirection vers la page de login
-    };
+			navigate("/connexion");
+		};
 
-    handleLogout();
-  }, []);
+		handleLogout();
+	}, []);
 
-  return null; // Pas besoin d'afficher quoi que ce soit
+	return null; // Pas besoin d'afficher quoi que ce soit
 };
 
 export default Logout;
