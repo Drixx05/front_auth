a3da196b8edfc6d2448086699ac588f79413e777
Antonin Grenier <drixion05@gmail.com>
2025-11-19 12:07:23 +0100

    added display logic on Header compo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,53 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import "../assets/styles/Header.css";
 function Header() {
-  return (
-    <Navbar bg="light" data-bs-theme="light">
-      <Container>
-        <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/">
-            Accueil
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/publiques">
-            Offres Publiques
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/professionnelles">
-            Offres Professionnelles
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/inscription">
-            Inscription
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/connexion">
-            Connexion
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/deconnexion">
-            Déconnexion
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+	const location = useLocation();
+
+	const getValidToken = () => {
+		const auth = JSON.parse(localStorage.getItem("auth"));
+		const isValid = auth && new Date(auth.expiresAt) > new Date();
+		return isValid;
+	};
+
+	const [isConnected, setIsConnected] = useState(false);
+
+	useEffect(() => {
+		setIsConnected(getValidToken());
+	}, [location]);
+
+	return (
+		<Navbar bg="light" data-bs-theme="light">
+			<Container>
+				<Nav className="ms-auto">
+					<Nav.Link as={NavLink} to="/">
+						Accueil
+					</Nav.Link>
+					<Nav.Link as={NavLink} to="/offres/publiques">
+						Offres Publiques
+					</Nav.Link>
+
+					<Nav.Link as={NavLink} to="/offres/professionnelles">
+						Offres Professionnelles
+					</Nav.Link>
+					{!isConnected && (
+						<Nav.Link as={NavLink} to="/inscription">
+							Inscription
+						</Nav.Link>
+					)}
+					{!isConnected && (
+						<Nav.Link as={NavLink} to="/connexion">
+							Connexion
+						</Nav.Link>
+					)}
+					{isConnected && (
+						<Nav.Link as={NavLink} to="/deconnexion">
+							Déconnexion
+						</Nav.Link>
+					)}
+				</Nav>
+			</Container>
+		</Navbar>
+	);
 }
 export default Header;
