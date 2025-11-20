4478453b381b96c6bd0b0579eba8fb9a4acd2b5c
Antonin Grenier <drixion05@gmail.com>
2025-11-20 11:55:06 +0100

    first commit

--- src/pages/OfferProList.jsx
+++ src/pages/OfferProList.jsx
@@ -3,56 +3,56 @@ import { Spinner, Alert } from "react-bootstrap";
 import OfferList from "../components/OfferList.jsx";
 
 const OfferProList = () => {
-  const [offers, setOffers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+	const [offers, setOffers] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(
-          "https://offers-api.digistos.com/api/offers/pro",
-          {
-            headers: {
-              Accept: "application/json",
-              // Add Authorization token
-            },
-          }
-        );
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await fetch(
+					"https://offers-api.digistos.com/api/offers/pro",
+					{
+						headers: {
+							Accept: "application/json",
+							credentials: "include", // permet au navigateur de recevoir et stocker le cookie HttpOnly
+						},
+					}
+				);
 
-        const { data: offers, message } = await response.json();
-        if (!response.ok) {
-          throw { status: response.status, message: message };
-        }
+				const { data: offers, message } = await response.json();
+				if (!response.ok) {
+					throw { status: response.status, message: message };
+				}
 
-        setOffers(offers);
-      } catch (err) {
-        if (err.status === 401) {
-          setError("Vous n'êtes pas autorisé à accéder aux offres (401).");
-        } else {
-          setError("Une erreur est survenue lors du chargement des offres.");
-        }
-        console.error(err.message || err);
-      } finally {
-        setLoading(false);
-      }
-    };
+				setOffers(offers);
+			} catch (err) {
+				if (err.status === 401) {
+					setError("Vous n'êtes pas autorisé à accéder aux offres (401).");
+				} else {
+					setError("Une erreur est survenue lors du chargement des offres.");
+				}
+				console.error(err.message || err);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    fetchProducts();
-  }, []);
+		fetchProducts();
+	}, []);
 
-  if (loading) {
-    return <Spinner animation="border" className="d-block mx-auto mt-5" />;
-  }
-  if (error) {
-    return (
-      <Alert variant="danger" className="mt-5 text-center">
-        {error}
-      </Alert>
-    );
-  }
+	if (loading) {
+		return <Spinner animation="border" className="d-block mx-auto mt-5" />;
+	}
+	if (error) {
+		return (
+			<Alert variant="danger" className="mt-5 text-center">
+				{error}
+			</Alert>
+		);
+	}
 
-  return <OfferList offers={offers} />;
+	return <OfferList offers={offers} />;
 };
 
 export default OfferProList;
